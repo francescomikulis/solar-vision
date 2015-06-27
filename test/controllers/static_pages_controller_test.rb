d5cbd5e527fc8fad254604650f1945bbcffc4afd
Francesco Alessandro Stefano Mikulis-Borsoi <francescomikulis@yahoo.com>
2015-06-27 15:52:47 +0000

    Finished static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Francesco Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Francesco Sample App"
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Francesco Sample App"
+  end
 end

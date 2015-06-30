b17b2efe816948e2e779270afadabc7b234fe1af
Francesco Alessandro Stefano Mikulis-Borsoi <francescomikulis@yahoo.com>
2015-06-30 21:23:31 +0000

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   
-  # Returns the fukk title on a per-page basis.
-  def full_title(page_title = "")
-    base_title = "Francesco Sample App"
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = 'Francesco Sample App'
     if page_title.empty?
       base_title
     else
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -19,4 +19,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | Francesco Sample App"
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Francesco Sample App"
+  end
 end

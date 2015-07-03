a0232437fee9d61db1e4c0f25027776a625d755d
Francesco Alessandro Stefano Mikulis-Borsoi <francescomikulis@yahoo.com>
2015-07-03 16:35:49 +0000

    Only I can edit MY info + friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  # Returns true if the given user is the same as the current_user
+  def current_user?(user)
+    user == current_user
+  end
+  
   # Returns the current user that is logged in
   def current_user
     if (user_id = session[:user_id]) #ASSIGNMENT
@@ -42,4 +47,17 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  # Redirects to a stored location (or default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+  
 end
+

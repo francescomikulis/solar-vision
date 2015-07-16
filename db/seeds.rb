332ac2b148dcbd9a097dbd24a8125cbebb2622d6
Francesco Alessandro Stefano Mikulis-Borsoi <francescomikulis@yahoo.com>
2015-07-16 18:23:07 +0000

    All is working

--- db/seeds.rb
+++ db/seeds.rb
@@ -58,9 +58,10 @@ users = User.order(:created_at).take(6)
 end
 
 # Following relationships
-users = User.all
-user = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |single_followed_user| user.follow(single_followed_user) }
-followers.each { |single_follower_user| single_follower_user.follow(user) }
+
+#users = User.all
+#user = users.first
+#following = users[2..50]
+#followers = users[3..40]
+#following.each { |single_followed_user| user.follow(single_followed_user) }
+#followers.each { |single_follower_user| single_follower_user.follow(user) }

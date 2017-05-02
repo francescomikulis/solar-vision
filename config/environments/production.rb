f28fa87f4c902fd7dd1d48d831b2d2f9598225e1
Francesco Alessandro Stefano Mikulis-Borsoi <fam1001@wildcats.unh.edu>
2017-05-02 01:55:48 +0000

    Removed AWS data

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'still-basin-8518.herokuapp.com'
+  host = 'agile-depths-75767.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
